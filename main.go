2159f856be2a8d418fb7e99b873f382735b9d6e5
IsCycleBai <180962954+IsCycleBai@users.noreply.github.com>
2025-01-26 04:58:43 +0000

    upd

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	http.HandleFunc("/", handler.Handler)
 
-	port := "3000"
+	port := "3018"
 	fmt.Printf("Server Start At http://localhost:%s\n", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		fmt.Printf("Server Failed: %v\n", err)
